b370537bb6382a77c956ac1497028d4f6d4f4f9d
Michal Jagiello <michal.jagiello@infullmobile.com>
2016-02-13 22:12:03 +0100

    Move static files deployment to Whitenose

--- rurki/settings.py
+++ rurki/settings.py
@@ -129,4 +129,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 from .local_settings import *
